5f3fa0001df0a2b57c5f9e7b1293ec95732f3dc3
HABBIE-MARIA-EUGENE <meugenem1409@gmail.com>
2026-06-15 00:47:58 +0530

    primeCheckV2

--- Python basics/fundamentals/primeCheck.py
+++ Python basics/fundamentals/primeCheck.py
@@ -1,18 +1,14 @@
-# V1
-n = 5
-
+# V2
+n = 116
 count = 0
 
 for i in range (1,n+1):
 
     if (n%i==0):
         count+=1
-
-    else:
-        continue
-
+        if count>2:
+            break
 print(count)
-
 if count == 2:
     print("prime")
 else:
