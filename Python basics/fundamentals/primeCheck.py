fa380f869c167bdbaa1d4d3eca18567cee3be990
HABBIE-MARIA-EUGENE <meugenem1409@gmail.com>
2026-06-15 00:34:29 +0530

    prime num check

---
+++ Python basics/fundamentals/primeCheck.py
@@ -0,0 +1,19 @@
+# V1
+n = 5
+
+count = 0
+
+for i in range (1,n+1):
+
+    if (n%i==0):
+        count+=1
+
+    else:
+        continue
+
+print(count)
+
+if count == 2:
+    print("prime")
+else:
+    print("Composite")
